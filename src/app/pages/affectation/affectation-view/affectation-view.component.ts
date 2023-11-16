d0ef65c44a22ff2f213377546d76304e56bf761d
Muanza Kangudie <muanza.kangudie@gmail.com>
2023-11-16 16:39:52 +0100

    affectations

--- src/app/pages/affectation/affectation-view/affectation-view.component.ts
+++ src/app/pages/affectation/affectation-view/affectation-view.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NotifierService } from 'angular-notifier';
 import { Affectation } from 'src/app/models/affectation.model';
 import { JarvisService } from 'src/app/services/jarvis.service';
+import { Location } from '@angular/common';
+import { Vigile } from 'src/app/models/vigile.model';
+import { VigileService } from 'src/app/services/vigile.service';
 
 @Component({
   selector: 'app-affectation-view',
@@ -12,12 +15,18 @@ import { JarvisService } from 'src/app/services/jarvis.service';
 export class AffectationViewComponent implements OnInit {
 
   affectation = new Affectation();
+  date = new Date();
+  remplacant = new Vigile();
+  vigiles = new Array<Vigile>();
+  rechercheVigile = "";
 
   constructor(
+    private _location: Location,
     private route: ActivatedRoute,
     private router: Router,
     private notifierService: NotifierService,
     private affectationService: JarvisService<Affectation>,
+    private vigileService: VigileService,
   ) { }
 
   ngOnInit(): void {
@@ -28,6 +37,9 @@ export class AffectationViewComponent implements OnInit {
           console.log('le affectation recupéré');
           console.log(affectation);
           this.affectation = affectation;
+          this.date = this.affectation.dateAffectation;
+          this.remplacant = this.affectation.remplacant;
+          this.vigiles = [this.remplacant];
         });
       } else {
         this.affectation.dateAffectation = new Date().toISOString().split('T')[0];
@@ -40,12 +52,39 @@ export class AffectationViewComponent implements OnInit {
     this.affectation.arret = new Date(this.affectation.arret);
     this.affectationService.modifier('affectation', this.affectation.idaffectation, this.affectation).then(() => {
       this.notifierService.notify('success', "Affectation mise en arrêt avec succès");
-        this.router.navigate(['affectation']);
+      this.router.navigate(['affectation']);
     });
+  }
+
+  modifierDate() {
+    this.affectation.dateAffectation = new Date(this.date);
+    this.affectationService.modifier('affectation', this.affectation.idaffectation, this.affectation).then(() => {
+      this.notifierService.notify('success', "Date de l'affectation mise à jour avec succès");
+      this.retour();
+    });
+  }
+
+  modifierRemplacant() {
+    this.affectation.remplacant = this.remplacant,
+      this.affectationService.modifier('affectation', this.affectation.idaffectation, this.affectation).then(() => {
+        this.notifierService.notify('success', "Remplacant mis à jour avec succès");
+        this.retour();
+      });
+  }
+
+  getVigiles(texte: string) {
+    if (texte.length > 4)
+      this.vigileService.rechercheCalme(texte).then((vigiles) => {
+        this.vigiles = vigiles;
+      });
   }
 
   jourSemaine(jour: number | string) {
     return this.affectationService.jourSemaine(Number(jour))
+  }
+
+  retour() {
+    this._location.back();
   }
 
 }
